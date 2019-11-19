f3d505cd988fe3be5853c8e35e8c0f52a4dc2b4e
hmehta46 <hmehta46@in.ibm.com>
2019-11-19 15:46:51 +0530

    Update shipping-controller.js
    changing the price

--- src/controllers/shipping-controller.js
+++ src/controllers/shipping-controller.js
@@ -2,8 +2,8 @@ var productService = require("../services/product-service");
 
 class ShippingController {
   constructor() {
-    this.REGULAR_PRICE = 0.1;
-    this.OVERNIGHT_PRICE = 1;
+    this.REGULAR_PRICE = 0.8;
+    this.OVERNIGHT_PRICE = 5;
   }
 
   async getItemShipping(item) {
